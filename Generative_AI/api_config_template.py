0aa34eaff67c914788582f94a31bfdc0b5d2990c
Yasmeen-Otyfah <yasmeen881122@gmail.com>
2026-05-09 17:52:51 +0300

    Create api_config_template.py

---
+++ Generative_AI/api_config_template.py
@@ -0,0 +1,13 @@
+# ==========================================
+# API Configuration Settings
+# ==========================================
+# Note: For security reasons, the actual API key is managed via Google Colab's userdata (Secrets).
+# Do NOT hardcode or upload the real API key to GitHub.
+
+API_SETTINGS = {
+    "PROVIDER": "Groq",
+    "MODEL_NAME": "llama-3.1-8b-instant",
+    "API_KEY": "YOUR_GROQ_API_KEY_HERE", # Replace securely in production
+    "MAX_TOKENS": 800,
+    "TEMPERATURE": 0.7
+}
